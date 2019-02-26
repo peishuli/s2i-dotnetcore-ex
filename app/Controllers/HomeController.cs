39be4263db0a096c18804e0150cdea58f787fba5
Li Peishu <peishuli62@gmail.com>
2019-02-26 17:00:59 -0600

    modified About page

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace app.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "About Minishift???";
 
             return View();
         }
